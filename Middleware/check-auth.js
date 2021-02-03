eb7b9bae2c7cad161a3aeb1ad826d7fece6a3d97
jrcamilon <ecamilon@infosol.com>
2021-02-02 20:44:40 -0700

    Installed Firebase Admin SDK and added in middleware

--- Middleware/check-auth.js
+++ Middleware/check-auth.js
@@ -1,4 +1,5 @@
-const jwt = require('json-web-token')
+const jwt = require('json-web-token');
+var admin = require("firebase-admin");
 
 module.exports = (req, res, next) => {
 
@@ -7,33 +8,30 @@ module.exports = (req, res, next) => {
      * and check access token against Firebase 
      * if accessToken is valid do next() else
      * return 403 error
+     * https://firebase.google.com/docs/auth/admin/verify-id-tokens#web
      */
 
      try {
         // REACH OUT TO FIREBASE WITH ACCESS TOKEN FROM USER
         // check if access token is valid. if valid next(), else res.status(403).send({message: 'No Access Token, or access token invalid'})
-        let accessToken = req.cookies.jwt
-
-        //if there is no token stored in cookies, the request is unauthorized
-        if (!accessToken){
-            return res.status(403).send()
-        }
-    
-        let payload
-        try{
-            //use the jwt.verify method to verify the access token
-            //throws an error if the token has expired or has a invalid signature
-            payload = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
+        const idToken = req.headers.authorization.split(" ")[1];
+        
+        admin
+        .auth()
+        .verifyIdToken(idToken)
+        .then((decodedToken) => {
+            const uid = decodedToken.uid;
+            console.log(uid);
             next()
-        }
-        catch(e){
-            //if an error occured return request unauthorized error
-            return res.status(401).send()
-        }
-      //   next();
+        })
+        .catch((error) => {
+            console.log(error)
+            // Handle error
+            res.status(401).send({message: 'User Not Authenticated with Firebase'});
+        });
+
      } catch (error) {
-        next();
-        // res.status(403).send({message: 'No Access Token, or access token invalid'})
+        res.status(403).send({message: 'No Access Token, or access token invalid'})
      }
 
 }
